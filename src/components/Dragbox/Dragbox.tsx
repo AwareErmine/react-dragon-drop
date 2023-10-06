e4083c1d863aff264d435f9d3b402257cb006623
awareermine <aramie.ewen@gmail.com>
2023-10-06 06:06:19 -0400

    add tsconfig

--- src/components/Dragbox/Dragbox.tsx
+++
@@ -1,3 +0,0 @@
-export default function Dragbox({}) {
-    return 
-}
--- src/components/Draggable/Draggable.tsx
+++ src/components/Draggable/Draggable.tsx
@@ -1,20 +1,21 @@
 import React from "react";
 
-const onDrag = (event) => event.dataTransfer.setData("dragged-id", event.target.id);
-const onDrop = (event) => {
+const onDrag = (event: React.DragEvent) => event.dataTransfer.setData("dragged-id", (event.target as HTMLElement).id);
+const onDrop = (event: React.DragEvent) => {
     event.preventDefault();
     const dragged = document.getElementById(event.dataTransfer.getData("dragged-id"));
-    if (dragged && event.currentTarget.offsetTop > dragged.offsetTop) {
-        event.currentTarget.parentNode.insertBefore(dragged, event.currentTarget);
-    } else {
-        event.currentTarget.after(dragged);
+    const target = event.currentTarget as HTMLElement;
+    if (dragged && target.offsetTop > dragged.offsetTop) {
+        target.parentNode?.insertBefore(dragged, target);
+    } else if (dragged) {
+        target.after(dragged);
     }
 }
 
 export const Draggable: React.FC<React.PropsWithChildren> = ({children}) => {
     return (
         <div 
-            draggable="true" 
+            draggable={true} 
             onDragStart={onDrag}
             onDragEnd={onDrop}
             id={Date.now().toPrecision()}
