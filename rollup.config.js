571f48723200a3a4e95ffadeb4c8ad0c510045d5
awareermine <aramie.ewen@gmail.com>
2023-10-09 10:37:04 -0400

    maybe this is it

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import { terser } from "rollup-plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 const packageJson = require("./package.json");
@@ -29,7 +28,6 @@ export default [
       }),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      terser(),
     ],
     external: ["react", "react-dom", "styled-components"],
   },
