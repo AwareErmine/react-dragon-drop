20c513b2c2735cc89fc36726974a04c1889d7877
awareermine <aramie.ewen@gmail.com>
2023-10-09 12:18:55 -0400

    make assets folder

--- src/components/Draggable/Draggable.tsx
+++ src/components/Draggable/Draggable.tsx
@@ -70,7 +70,7 @@ const Dragon: React.FC<{
 
     return (
         <img 
-            src={require("./dragon.gif")} 
+            src={require("/assets/dragon.gif")} 
             id={"dragon" + draggableId}
             alt="dragon" 
             style={{
